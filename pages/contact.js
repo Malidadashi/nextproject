d296fcdda7282ca31cf10bdf9b8efe5d7af32fb5
malidadashi <m.alidadashi92@yahoo.com>
2024-06-28 09:57:10 +0330

    contact

--- pages/contact.js
+++ pages/contact.js
@@ -45,14 +45,14 @@ const ContactOptions = () => {
             <p className='text-xl mt-4'>Socials : </p>
 
             <div className='flex items-center justify-between mt-4 mb-10 pr-2 justify-self-end'>
-                <a className='p-1 rounded-lg border-2 border-transparent hover:border-white flex items-center gap-x-2 transition-all duration-150 group' target='_blank' href="linkedin.com/in/arsi-gh/"><FaLinkedin  className='text-3xl'/><p className='text-[0px] group-hover:text-base transition-all duration-300 delay-75'>Linkedin</p></a>
-                <a className='p-1 rounded-lg border-2 border-transparent hover:border-white flex items-center gap-x-2 transition-all duration-150 group' href=""><FaInstagram  className='text-3xl'/><p className='text-[0px] group-hover:text-base transition-all duration-300 delay-75'>Instagram</p></a>
-                <a className='p-1 rounded-lg border-2 border-transparent hover:border-white flex items-center gap-x-2 transition-all duration-150 group' href=""><FaTelegramPlane  className='text-3xl'/><p className='text-[0px] group-hover:text-base transition-all duration-300 delay-75'>Telegram</p></a>
-                <a className='p-1 rounded-lg border-2 border-transparent hover:border-white flex items-center gap-x-2 transition-all duration-150 group' target='_blank' href="github.com/Arsi-gh"><FaGithub  className='text-3xl'/><p className='text-[0px] group-hover:text-base transition-all duration-300 delay-75'>Github</p></a>
+                <a className='p-1 rounded-lg border-2 border-transparent hover:border-white flex items-center gap-x-2 transition-all duration-150 group' target='_blank' href="https://www.linkedin.com/in/mohammad-ali-dadashi"><FaLinkedin  className='text-3xl'/><p className='text-[0px] group-hover:text-base transition-all duration-300 delay-75'>Linkedin</p></a>
+                <a className='p-1 rounded-lg border-2 border-transparent hover:border-white flex items-center gap-x-2 transition-all duration-150 group' href="@M.alidadashi"><FaInstagram  className='text-3xl'/><p className='text-[0px] group-hover:text-base transition-all duration-300 delay-75'>Instagram</p></a>
+                <a className='p-1 rounded-lg border-2 border-transparent hover:border-white flex items-center gap-x-2 transition-all duration-150 group' href="@Malidadashi"><FaTelegramPlane  className='text-3xl'/><p className='text-[0px] group-hover:text-base transition-all duration-300 delay-75'>Telegram</p></a>
+                <a className='p-1 rounded-lg border-2 border-transparent hover:border-white flex items-center gap-x-2 transition-all duration-150 group' target='_blank' href="https://github.com/Malidadashi"><FaGithub  className='text-3xl'/><p className='text-[0px] group-hover:text-base transition-all duration-300 delay-75'>Github</p></a>
             </div>
             <div className='flex flex-col gap-3'>
-                <a className='rounded-lg border-2 border-white p-2 flex justify-between items-center' href="mailto:arsalanghoochani15@gmail.com">Send an email<MdAlternateEmail className='text-2xl'/></a>
-                <a className='rounded-lg border-2 border-white p-2 flex justify-between items-center' href="tel:09136955525">Contact phone<TbPhone className='text-2xl'/></a>
+                <a className='rounded-lg border-2 border-white p-2 flex justify-between items-center' href="m.alidadashi92@YAHOO.COM.com">Send an email<MdAlternateEmail className='text-2xl'/></a>
+                <a className='rounded-lg border-2 border-white p-2 flex justify-between items-center' href="tel:091124338">Contact phone<TbPhone className='text-2xl'/></a>
             </div>
         </div>
         </>
